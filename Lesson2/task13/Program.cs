ba8716d9eb997b76bba3eac848d1439dffcdcef4
geo <i@gpolkanov.ru>
2022-08-28 17:22:31 +0300

    Урок 2 Задача №13 решена

---
+++ Lesson2/task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string a = Convert.ToString(number);
+    if(a.Length<2)
+    {
+        Console.WriteLine("Это третьей цифры нет ");
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра числа равна " + a[2]);
+    }
+    
