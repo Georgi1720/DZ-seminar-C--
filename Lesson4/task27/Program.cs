184632e829b61201eaa7c41ba991b8f67d194c23
geo <i@gpolkanov.ru>
2022-09-01 19:51:28 +0300

    Исправил №27

---
+++ Lesson4/task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+string numb = Console.ReadLine();
+int sum =0;
+for(int i=0;i<numb.Length;i++)
+{
+    int cifr =numb[i]-'0';
+    sum=sum+cifr;
+}
+
+Console.WriteLine(sum);
