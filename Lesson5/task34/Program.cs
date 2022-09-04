746a4b1dc84659763edda96a9550746b9e04cdad
geo <i@gpolkanov.ru>
2022-09-04 15:43:08 +0300

    Урок 5 задача №38 решена

---
+++ Lesson5/task34/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] array = new int [3];
+Random rnd = new Random();
+int count = 0;
+   
+   for(int i=0;i<array.Length;i++)   
+   {
+       array[i]=rnd.Next(100,999);
+       if(i==0)
+        {
+            Console.Write($"[ {array[i]}, ");
+        }
+       else if(i>=1 && i<=array.Length-2)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+       else 
+        {
+            Console.Write($"{array[i]} ] ");
+        }
+   }
+   
+   for (int g = 0;g<array.Length;g++)   // прошелся по массиву с проверкой на чётность 
+   {
+       if(array[g]%2==0) count = count+1;
+
+   }
+Console.WriteLine();
+Console.WriteLine($"В этом массиве четных чисел: {count}шт");
---
+++ Lesson5/task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+//стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+ int [] array = new int [5];
+   Random rnd= new Random();
+   int sumnechet = 0;
+   
+   for(int i=0;i<array.Length;i++)
+   {
+       array[i]=rnd.Next(1,50);
+       if (i==0)
+       {
+           Console.Write($"[ {array[i]},");
+       }
+       else if (i>=1 && i<=array.Length-2)
+       {
+           Console.Write($" {array[i]},");
+       }
+       else
+       {
+           Console.Write($" {array[i]} ]");
+       }
+   }
+   
+   for(int g = 0;g<array.Length;g++)
+   {
+        if(g==0)
+        {
+            
+        }
+        else if(g%2 != 0) sumnechet+=array[g];
+   }
+   
+   Console.WriteLine();
+   Console.WriteLine($"Сумма нечетных позиций массива равна: {sumnechet}");
